9aa743af58db06db2edf4d8072d15e2740487746
Younesabualchay69 <93259343+Younesabualchay69@users.noreply.github.com>
2023-03-14 20:37:05 +0100

    Kart

--- app.rb
+++ app.rb
@@ -45,8 +45,26 @@ end
 
 
 get('/cart') do
-    slim(:"cart/index", locals:{user:getAnv()})
+    usr = getAnv()
+    db = grab_db()
+    cart = db.execute("SELECT * FROM cart WHERE user_id=?", usr["id"])
+
+    slim(:"cart/index", locals:{user:getAnv(), cart:cart})
 end  
+
+
+post("/cart") do
+    usr_id = session[:id].to_i
+    prod_id = params["product_id"]
+    db = grab_db()
+    result = db.execute("SELECT * FROM cart WHERE user_id=? AND product_id=?", usr_id, prod_id)
+    if result.length > 0 then
+      db.execute("UPDATE cart SET items = ? WHERE user_id=? AND product_id=?", result[0]["items"] + 1, usr_id, prod_id)
+    else
+      db.execute("INSERT INTO cart (user_id,product_id) VALUES (?,?)", usr_id, prod_id)
+    end
+    redirect('/products')
+end
 
 get('/profile') do 
 
